6a950e047723e7c111ae0c481d580f8ca330c1f3
ArtInLines <linesinart@gmail.com>
2020-07-03 23:29:44 +0200

    Calculate all possible solutions instead of just the first solution
    Also changed tests

--- solve.js
+++ solve.js
@@ -9,16 +9,28 @@ const empty = null;
 // <-- TESTS --> //
 //               //
 // Many possible solutions
+const board0 = [
+	[1, 2, 3, empty, empty, empty, empty, empty, empty],
+	[empty, empty, empty, empty, empty, empty, empty, empty, 4],
+	[empty, empty, empty, empty, empty, empty, empty, empty, 5],
+	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
+	[empty, empty, empty, empty, 8, empty, empty, empty, empty],
+	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
+	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
+	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
+	[empty, empty, empty, empty, empty, empty, empty, empty, 9],
+];
+
 const board1 = [
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[1, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
+	[5, empty, empty, empty, 7, empty, empty, empty, empty],
+	[6, empty, empty, 1, 9, 5, empty, empty, empty],
+	[empty, 9, 8, empty, empty, empty, empty, empty, empty],
+	[empty, empty, empty, empty, empty, empty, empty, empty, 3],
+	[4, empty, empty, 8, empty, 3, empty, empty, 1],
+	[7, empty, empty, empty, 2, empty, empty, empty, empty],
+	[empty, 6, empty, empty, empty, empty, 2, 8, empty],
+	[empty, empty, empty, 4, 1, 9, empty, empty, 5],
+	[empty, empty, empty, empty, 8, empty, empty, 7, 9],
 ];
 
 // One solution
@@ -47,14 +59,16 @@ const board3 = [
 	[empty, empty, empty, empty, empty, empty, empty, empty, 9],
 ];
 
+let solutions = [];
+
 function solve(board) {
 	if (solved(board)) {
-		return board;
-	} else {
-		const possibilities = nextBoards(board);
-		const validBoards = keepOnlyValid(possibilities);
-		return searchForSolution(validBoards);
+		solutions.push(board);
+		console.log(board, solutions.length);
 	}
+	const possibilities = nextBoards(board);
+	const validBoards = keepOnlyValid(possibilities);
+	return searchForSolution(validBoards);
 }
 
 function searchForSolution(boards) {
@@ -211,7 +225,8 @@ function boxesGood(board) {
 	return true;
 }
 
-//               //
-// <-- TESTS --> //
-console.log(solve(board2));
-//               //
+// ##      ## //
+// ## TEST ## //
+// ##      ## //
+// solve(board0);
+// console.log('Final length:', solutions.length);
