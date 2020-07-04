ddf804834d4b0c76eaea654bb597a009b88102f5
ArtInLines <linesinart@gmail.com>
2020-07-04 10:28:19 +0200

    Added comment to create.js

--- public/assets/create.mjs
+++ public/assets/create.mjs
@@ -2,17 +2,8 @@
 import main from './solve.mjs';
 
 const empty = null;
-const board = [
-	[5, 3, empty, empty, 7, empty, empty, empty, empty],
-	[6, empty, empty, 1, 9, 5, empty, empty, empty],
-	[empty, 9, 8, empty, empty, empty, empty, 6, empty],
-	[8, empty, empty, empty, 6, empty, empty, empty, 3],
-	[4, empty, empty, 8, empty, 3, empty, empty, 1],
-	[7, empty, empty, empty, 2, empty, empty, empty, 6],
-	[empty, 6, empty, empty, empty, empty, 2, 8, empty],
-	[empty, empty, empty, 4, 1, 9, empty, empty, 5],
-	[empty, empty, empty, empty, 8, empty, empty, 7, 9],
-];
+
+// Add script to create a sudoku
 
 const result = main(board);
 
